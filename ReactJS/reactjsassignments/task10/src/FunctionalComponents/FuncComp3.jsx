dccf3f6550f62d0c3fac3faba6facd7e9f09b594
siva5965 <sivacoolkalyan@gmail.com>
2022-12-12 02:39:07 +0530

    adding apps

---
+++ ReactJS/reactjsassignments/task10/src/FunctionalComponents/FuncComp3.jsx
@@ -0,0 +1,6 @@
+function FuncCode3(){
+    return <div>
+        <h2>3. Welcome to Functional Components.</h2>
+    </div>
+}
+export default FuncCode3
---
+++ ReactJS/reactjsassignments/task10/src/FunctionalComponents/FuncComp9.jsx
@@ -0,0 +1,6 @@
+function FuncCode9(){
+    return <div>
+        <h2>9. Welcome to Functional Components.</h2>
+    </div>
+}
+export default FuncCode9
