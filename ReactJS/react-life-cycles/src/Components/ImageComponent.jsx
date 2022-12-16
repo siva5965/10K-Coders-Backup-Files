32b26ba3a5731beedd7fa87c63d053492d2d82b0
siva5965 <sivacoolkalyan@gmail.com>
2022-12-17 01:45:57 +0530

    add

--- ReactJS/react-life-cycles/src/Components/ImageComponent.jsx
+++ ReactJS/react-life-cycles/src/Components/ImageComponent.jsx
@@ -2,20 +2,22 @@ import React, { Component } from 'react'
 
 export default class ImageComponent extends Component {
     constructor(props) {
-      super(props)
-    
-      this.state = {
-         
-      }
-    }
-    
-  render() {
-    return (
-      <div>
-        {this.props.show && <img src="https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8=" alt="" width={200}/>}
-      </div>
-    )
-  }
+        super(props)
 
-  
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.show && <img src="https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8=" alt="" width={200} />}
+            </div>
+        )
+    }
+    componentWillUnmount() {
+        console.log("This component is going to be Removed from DOM")
+    }
+
 }
