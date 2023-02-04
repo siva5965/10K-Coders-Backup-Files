6d60f2090087ff62b64975b4b7850781353187ae
siva5965 <sivacoolkalyan@gmail.com>
2023-02-04 13:56:07 +0530

    add

--- Projects/calculator-app/src/Components/App1.jsx
+++ Projects/calculator-app/src/Components/App1.jsx
@@ -15,26 +15,6 @@ const App1 = () => {
                 <button type="button" className="btn btn-success" onClick={()=> setResult(eval(input))}>Result</button>
                 <h4>Result is : {result}</h4>
 
-                {/* <button onClick={()=> setInput(input+'+')}>+</button>
-                <button onClick={()=> setInput(input+'-')}>-</button>
-                <button onClick={()=> setInput(input+'*')}>*</button>
-                <button onClick={()=> setInput(input+'/')}>/</button>
-                <button onClick={()=> setInput('')}>clr</button> <br />
-
-                <button onClick={()=> setInput(input+'1')}>1</button>
-                <button onClick={()=> setInput(input+'2')}>2</button>
-                <button onClick={()=> setInput(input+'3')}>3</button> <br />
-                <button onClick={()=> setInput(input+'4')}>4</button>
-                <button onClick={()=> setInput(input+'5')}>5</button> 
-
-                <button onClick={()=> setInput(input+'6')}>6</button> <br />
-                <button onClick={()=> setInput(input+'7')}>7</button>
-                <button onClick={()=> setInput(input+'8')}>8</button>
-                <button onClick={()=> setInput(input+'9')}>9</button> <br />
-                <button onClick={()=> setInput(input+'0')}>0</button>
-                <button onClick={()=> setInput(input+'.')}>.</button>
-                <button onClick={()=> setResult(eval(input))}>=</button> */}
-
                 <button type="button" className="btn btn-secondary" onClick={()=> setInput(input+'9')}>9</button>
                 <button type="button" className="btn btn-secondary" onClick={()=> setInput(input+'8')}>8</button>
                 <button type="button" className="btn btn-secondary" onClick={()=> setInput(input+'7')}>7</button>
